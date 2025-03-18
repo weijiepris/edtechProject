e3a0774413aafa6438a4e44785bd173dcbe080fd
weijie <weijie@mavericks-consulting.com>
2025-03-18 14:14:37 +0800

    modify expo

---
+++ edtech-expo/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "app/index.js",
+    "./App.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
